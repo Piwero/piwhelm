331dcafb6090c68b18b2a46915d52de99d6045cc
Piwero <piwero@gmail.com>
2024-05-01 08:46:16 +0100

    feat(action-semantic-release): Set up new GH action for semantic releases

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    branches: [
+        {name: 'main'},
+    ],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/changelog", {"changelogFile": "CHANGELOG.md"}],
+        ["@semantic-release/git",
+            {
+                "assets": ["CHANGELOG.md"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }],
+        ["@semantic-release/github", {"addReleases": "bottom"}]
+    ],
+    publish: [
+        "@semantic-release/github"
+    ]
+};

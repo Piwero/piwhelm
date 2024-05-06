116917c593e9314c1ee2413b4a95faeb0a99830e
Piwero <piwero@gmail.com>
2024-05-06 17:28:19 +0100

    Revert "ci(helm-releaser): Set up for helm releases"
    This reverts commit a0323f7973229e7e4f5b335d00eaf40f5e7a4b0d.

--- release.config.js
+++ release.config.js
@@ -1,59 +1,19 @@
-const yaml = require('js-yaml');
-const Handlebars = require('handlebars')
-
-Handlebars.registerHelper("yamlString", function (options) {
-  const input = options.fn(this).trim()
-  return yaml.dump(input.replace(/^\s*\*\s*/g, '').trim(), {lineWith: 999, forceQuotes: true, quotingType: '"'});
-});
-
-
 module.exports = {
-  "branches": [
-    "main",
-  ],
-  "tagFormat": process.env.CHART_NAME + "-v${version}",
-  "plugins": [
-    [
-      "@semantic-release/commit-analyzer",
-      {}
+    branches: [
+        {name: 'main'},
     ],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        "writerOpts": {
-          "mainTemplate": "{{#each commitGroups}}{{#each commits}} - {{#yamlString}}{{> commit root=@root}}{{/yamlString}}{{/each}}{{/each}}",
-        }
-      }
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        ["@semantic-release/changelog", {"changelogFile": "CHANGELOG.md"}],
+        ["@semantic-release/git",
+            {
+                "assets": ["CHANGELOG.md"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }],
+        ["@semantic-release/github", {"addReleases": "bottom"}]
     ],
-    [
-      "@semantic-release/changelog",
-      {
-        "changelogFile": "CHANGELOG.md"
-      }
-    ],
-    [
-      "@semantic-release/exec",
-      {
-        "prepareCmd": "helm-docs",
-        "publishCmd": "cr package . && cr upload -o halkeye -r helm-charts -c $(git rev-parse HEAD) --release-notes-file CHANGELOG.md --push --packages-with-index --skip-existing"
-      }
-    ],
-    [
-      "semantic-release-helm3",
-      {
-        "chartPath": ".",
-        "populateChangelog": "true",
-        "onlyUpdateVersion": "true"
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        "assets": [
-          "Chart.yaml"
-        ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
+    publish: [
+        "@semantic-release/github"
     ]
-  ]
-}
+};
